abd49d31345c1397b6731524608aa4cbec7c3e28
Ben <ben.strumeyer@gmail.com>
2022-04-02 06:48:47 -0400

    Populate list with data

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -22,22 +22,7 @@ const CommentList = ({ mockComments }) => {
         <ListItemAvatar>
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         </ListItemAvatar>
-        <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
-        />
+        <ListItemText primary={comment.name} secondary={comment.comment} />
       </ListItem>
       <Divider variant="inset" component="li" />
     </div>
