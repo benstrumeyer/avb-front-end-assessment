4f164eb59137d7b59f83b230e9acb059e8f1f0f8
Ben <ben.strumeyer@gmail.com>
2022-04-02 02:59:30 -0400

    Add theme with primary and secondary colors

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
 import "app/App.css";
 import logo from "app/logo.svg";
 import Header from "components/Header";
 import CommentModal from "components/CommentModal";
 import CommentListContainer from "components/CommentListContainer";
+import { purple } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
 
       <CommentModal />
@@ -17,7 +28,7 @@ function App() {
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
