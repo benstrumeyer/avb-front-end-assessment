b8c7a58536c3024dc59770a8c8adc1172be7109e
Ben <ben.strumeyer@gmail.com>
2022-04-04 23:05:55 -0400

    Change top commenters to a responsive chip design

--- src/components/TopCommenters.js
+++ src/components/TopCommenters.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
@@ -14,21 +10,32 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     margin: theme.spacing(2),
   },
+  commenter: {
+    display: "flex",
+    alignItems: "center",
+  },
+  topCommenterContainer: {
+    display: "inline-flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    margin: "auto",
+    width: "100%",
+    flexWrap: "wrap",
+  },
 }));
 
 const CommentList = ({ mockComments }) => {
   const classes = useStyles();
-  const renderComments = mockComments.map((comment) => (
-    <div key={comment.id}>
-      <ListItem key={comment.id} alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt={comment.name} src="/static/images/avatar/1.jpg">
-            {comment.initials}
+  const renderTopCommenters = mockComments.map((commenter) => (
+    <div className={classes.commenter}>
+      <Chip
+        avatar={
+          <Avatar alt={commenter.name} src="/static/images/avatar/1.jpg">
+            {commenter.initials}
           </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary={comment.name} secondary={comment.comment} />
-      </ListItem>
-      <Divider variant="inset" component="li" />
+        }
+        label={`${commenter.name}: ${commenter.count}`}
+      />
     </div>
   ));
 
@@ -37,7 +44,8 @@ const CommentList = ({ mockComments }) => {
       <Typography variant="h6" className={classes.title}>
         Top 3 Commenters
       </Typography>
-      <List className={classes.root}>{renderComments}</List>
+      {/* <List className={classes.root}>{renderComments}</List> */}
+      <div className={classes.topCommenterContainer}>{renderTopCommenters}</div>
     </>
   );
 };
